fd04d8aa8a23209d307680d80e4cede34db85927
choi138 <kidjustinchoi@gmail.com>
2023-05-06 17:31:59 +0900

    feat(pages): add useForm in signOut page

--- src/pages/auth/signout/index.tsx
+++ src/pages/auth/signout/index.tsx
@@ -1,14 +1,46 @@
-import { isSignInVar } from '@/main';
+import { useForm } from 'react-hook-form';
+
+import * as S from './styled';
 
 export const SignOutPage: React.FC = () => {
-  const onClick = () => {
-    isSignInVar(true);
+  const {
+    register,
+    watch,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  // watch function watch the input value
+  const onSubmit = () => {
+    console.log(watch()); // You can only watch email by doing ('email') this
+  };
+
+  const onInValid = () => {
+    console.log(errors);
   };
   return (
-    <div>
+    <S.SignOutPageContainer>
       <span>This is sign out page</span>
       <br />
-      <button onClick={onClick}>Click to signIn</button>
-    </div>
+      <form onSubmit={handleSubmit(onSubmit, onInValid)}>
+        <div>
+          <input
+            {...register('email', {
+              required: "This field can't be empty",
+              validate: (email: string) => email.includes('@gmail.com'),
+            })}
+            type="email"
+            placeholder="email"
+          />
+        </div>
+        <div>
+          <input
+            {...register('password', { required: true })}
+            type="password"
+            placeholder="password"
+          />
+        </div>
+        <S.SignOutPageSinInButton>Submit</S.SignOutPageSinInButton>
+      </form>
+    </S.SignOutPageContainer>
   );
 };
---
+++ src/pages/auth/signout/styled.ts
@@ -0,0 +1,9 @@
+import styled from '@emotion/styled';
+
+export const SignOutPageContainer = styled.div``;
+
+export const SignOutPageSinInButton = styled.button`
+  border: none;
+  background-color: darkgrey;
+  color: white;
+`;
